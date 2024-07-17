dd15b132dde147ba09c28a157e25a6f56e30d2ff
akinloluwami <akinkunmioye42@gmail.com>
2024-07-17 13:13:48 +0100

    delete example project

--- example/routes/users/[id]/projects/[id]/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export const GET = (req: Request, res: Response) => {
-  const id = req.params.id;
-  res.send(`GET project = ${id}`);
-};
--- example/routes/users/[id]/projects/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export const GET = (req: Request, res: Response) => {
-  const userId = req.params.id;
-  res.send(`GET User ${userId} has projects`);
-};
