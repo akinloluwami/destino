0b4dabaf0c6f77c7adf612dd46dfd2a9a9d155b8
akinloluwami <akinkunmioye42@gmail.com>
2024-09-15 14:11:13 +0100

    Added colors for aesthetics.

--- src/middlewares/request-logger.ts
+++ src/middlewares/request-logger.ts
@@ -1,3 +1,4 @@
+import colors from "ansi-colors";
 import { NextFunction, Request, Response } from "express";
 
 export const requestLogger = (
@@ -12,8 +13,27 @@ export const requestLogger = (
       0
     );
     const statusCode = res.statusCode;
+
+    const methodColors: { [key: string]: (text: string) => string } = {
+      GET: colors.bgBlue.white,
+      POST: colors.bgGreen.white,
+      PUT: colors.bgYellow.white,
+      DELETE: colors.bgRed.white,
+    };
+
+    const methodStyler = methodColors[req.method] || colors.bgCyan.white;
+
+    const methodColor = methodStyler(req.method);
+    const statusColor =
+      statusCode >= 500
+        ? colors.redBright.bold(statusCode.toString())
+        : statusCode >= 400
+        ? colors.red.bold(statusCode.toString())
+        : colors.green.bold(statusCode.toString());
+    const timeColor = colors.magenta(`${durationInMilliseconds}ms`);
+
     console.log(
-      `[${req.method}]  ${req.originalUrl}  ${statusCode}  ${durationInMilliseconds}ms`
+      `${methodColor}  ${req.originalUrl}  ${statusColor}  ${timeColor}`
     );
   });
 
